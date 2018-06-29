e412c2aca3e36adc18edf4dca282cd27564c7b6c
awybin <awybin@gmail.com>
2018-06-29 19:22:06 -0300

    varias coisas

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -5,17 +5,30 @@
 
 int main(void) {
 	int tamTot = 958;
-	float perc = 0.33;// separaTreinoRand 0.33 = 84%	separaTreinoRandPosNeg 0.7 = 87%
+
+	//Alterando o percentual aqui pode-se selecionar diferentes tamanhos para o treinamento
+	float perc = 0.68;// Melhores resultados: separaTreinoRand 0.73 = 88.41%, separaTreinoRandPosNeg 0.68 = 88.27%
+
 	Arvore *a;
 	int tamTreino, tamTeste;
 
 	tamTreino = tamTot * perc;
 	tamTeste = tamTot - tamTreino;
 
-	separaTreinoRand(perc, tamTot, "data.txt", "conjTreino.csv", "conjTeste.csv");
-	//separaTreinoRandPosNeg(perc, tamTot, "data.txt", "conjTreino.txt", "conjTeste.txt");
+	/*Aqui estão as duas funcoes que podem separar os conjuntos
+	  para escolher, basta comentar uma e deixar a outra sem comentarios
+	*/
+	//separaTreinoRand(perc, tamTot, "data.txt", "conjTreino.txt", "conjTeste.txt");
+	separaTreinoRandPosNeg(perc, tamTot, "data.txt", "conjTreino.txt", "conjTeste.txt");
+	separaTreinoRandPosNeg(perc, tamTot, "data.txt", "conjTreino.csv", "conjTeste.csv");
+
+	/*As funcoes abaixo fazem a criacao, teste e comparacao dos resultados da arvore
+	  para testar usando todos os dados para o treinamento e para os testes basta
+	  comentar as funcoes de cima  e descomentar as de baixo
+	*/
 
 	a = criaArvore("conjTreino.txt", tamTreino);
+	//a = criaArvore("data.txt", tamTot);
 
 	testaArvore("conjTeste.txt", "resultado.txt", tamTeste, a);
 	//testaArvore("data.txt", "resultado.txt", tamTot, a);
